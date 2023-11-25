1f2b3bab8bf8650c56543fe3a2ad5e499ca5178d
Erazul Islam <erazul89@gmail.com>
2023-11-25 19:25:54 +0600

    create the form and make api

--- src/Components/CreateBio/CreateBio.jsx
+++ src/Components/CreateBio/CreateBio.jsx
@@ -1,99 +1,148 @@
-import Select from 'react-select'
+import axios from 'axios';
+
+import Swal from 'sweetalert2';
+
 
 const CreateBio = () => {
+    const handleAdd = e => {
+        e.preventDefault()
 
-    const type = [
-        { value: 'male', label: 'male' },
-        { value: 'female', label: 'female' },
+        const form = e.target;
 
-    ]
+        const bio = form.bio.value;
+        const image = form.img.value;
+        const type = form.type.value;
+        const birth = form.birth.value;
+        const height = form.height.value;
+        const weight = form.weight.value;
+        const age = form.age.value;
+        const occupation = form.occupation.value;
+        const fname = form.fname.value;
+        const mname = form.mname.value;
+        const perdiv = form.perdiv.value;
+        const prediv = form.prediv.value;
+        const exage = form.exage.value;
+        const exheight = form.exheight.value;
+        const exweight = form.exweight.value;
+        const mobile = form.mobile.value;
+        const email = form.email.value
 
-    const division = [
-        { value: 'Dhaka', label: 'Dhaka' },
-        { value: 'Barishal', label: 'Barishal' },
-        { value: 'Jessore', label: 'Jessore' },
-        { value: 'Sylhet', label: 'Sylhet' },
-        { value: 'Khulna', label: 'Khulna' },
+ 
 
-    ]
-    const present = [
-        { value: 'Dhaka', label: 'Dhaka' },
-        { value: 'Barishal', label: 'Barishal' },
-        { value: 'Jessore', label: 'Jessore' },
-        { value: 'Sylhet', label: 'Sylhet' },
-        { value: 'Khulna', label: 'Khulna' },
+        const AddBio = { bio,image,type,birth,height,weight,age,occupation,fname,mname,perdiv,prediv,exage,exheight,exweight,mobile,email }
+        console.log(AddBio)
 
-    ]
+        axios.post('http://localhost:5000/add', AddBio)
+            .then(res => {
+                console.log(res.data)
+                if (res.insertedId) {
+                    Swal.fire({
+                        title: "Good job!",
+                        text: "You clicked the button!",
+                        icon: "success"
+                    });
+                }
+            })
+
+    }
+
+
 
     return (
-        <div className='ml-10 w-56'>
-            <form>
-                <div>
-                    <Select name='type' options={type} />
-                    <div>
-                        <span className="">Your Name</span><br />
-                        <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="Your name" name="name" type="text" />
-                    </div>
-                    <div>
-                        <span className="">Photo</span><br />
-                        <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="Photo" name="photo" type="text" />
-                    </div>
-                    <div>
-                        <span className="">Date of Birth</span><br />
-                        <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="Date of birth" name="birth" type="text" />
-                    </div>
-                    <div>
-                        <span className="">Height</span><br />
-                        <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="Height" name="height" type="text" />
-                    </div>
-                    <div>
-                        <span className="">Weight</span><br />
-                        <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="weight" name="weight" type="text" />
-                    </div>
-                    <div>
-                        <span className="">Age</span><br />
-                        <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="age" name="age" type="text" />
-                    </div>
-                    <div>
-                        <span className="">Fathers Name</span><br />
-                        <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="father" name="father" type="text" />
-                    </div>
-                    <div>
-                        <span className="">Mothers Name</span><br />
-                        <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="mother" name="mother" type="text" />
-                    </div>
-                    <div>
-                        <span className="">Permanent</span><br />
-                        <Select name='type' options={division} />
-                    </div>
-                    <div>
-                        <span className="">Present</span><br />
-                        <Select name='type' options={present} />
-                    </div>
-                    <div>
-                        <span className="">Expected Partner age</span><br />
-                        <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="expected Age" name="exage" type="text" />
-                    </div>
-                    <div>
-                        <span className="">Expected partner height</span><br />
-                        <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="expected height" name="expheight" type="text" />
-                    </div>
-                    <div>
-                        <span className="">Expected partner weight</span><br />
-                        <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="expected weight" name="expweight" type="text" />
-                    </div>
-                    <div>
-                        <span className="">Email</span><br />
-                        <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="email" name="email" type="text" />
-                    </div>
-                    <div>
-                        <span className="">Phone</span><br />
-                        <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="Phone" name="Phone" type="text" />
-                    </div>
 
+        <div>
+            <h1 className="text-center pt-8 text-3xl">Add Bio</h1>
+            <form onSubmit={handleAdd} className="pl-36 mt-8 pb-8 pr-36">
+                <div className="grid grid-cols-1 lg:grid-cols-3 lg:justify-between">
+                    <div>
+                        <div>
+                            <span className="">Name</span><br />
+                            <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="Name" name="bio" type="text" />
+                        </div>
+                        <div>
+                            <span className="">Image</span><br />
+                            <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="Publish image" name="img" type="text" />
+                        </div>
+                        <div>
+                            <span className="">Type</span><br />
+                            <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="male/female" name="type" type="text" />
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <span className="">Date of birth</span><br />
+                            <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="Date of birth" name="birth" type="text" />
+                        </div>
+                        <div>
+                            <span className="">Height</span><br />
+                            <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="Height" name="height" type="text" />
+                        </div>
+                        <div>
+                            <span className="">Weight</span><br />
+                            <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="weight" name="weight" type="text" />
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <span className="">Age</span><br />
+                            <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="Age" name="age" type="text" />
+                        </div>
+                        <div>
+                            <span className="">Occupetion</span><br />
+                            <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="Ocuupation" name="occupation" type="text" />
+                        </div>
+                        <div>
+                            <span className="">Email</span><br />
+                            <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]"  required placeholder="email" name="email" type="text" />
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <span className="">Fathers name</span><br />
+                            <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="Fathers name" name="fname" type="text" />
+                        </div>
+                        <div>
+                            <span className="">Mothers name</span><br />
+                            <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="Mothers name" name="mname" type="text" />
+                        </div>
+                        <div>
+                            <span className="">Permanent Division</span><br />
+                            <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="permanent division" name="perdiv" type="text" />
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <span className="">Present Division</span><br />
+                            <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="Present division" name="prediv" type="text" />
+                        </div>
+                        <div>
+                            <span className="">Expected partner age</span><br />
+                            <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="Expected" name="exage" type="text" />
+                        </div>
+                        <div>
+                            <span className="">Expected partner height</span><br />
+                            <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="height" name="exheight" type="text" />
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <span className="">Expected partner weight</span><br />
+                            <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="Present division" name="exweight" type="text" />
+                        </div>
+                        <div>
+                            <span className="">Mobile Number</span><br />
+                            <input className="lg:w-96 mt-2 h-10 rounded bg-[#fff]" required placeholder="Expected" name="mobile" type="text" />
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <button className="btn w-full h-12 mt-4 rounded-lg hover:bg-yellow-600 bg-yellow-400 border-none text-[#331A15]">Add</button>
                 </div>
             </form>
         </div>
+
+
     );
 };
 
--- src/Components/DashBoard/DashboardCart.jsx
+++ src/Components/DashBoard/DashboardCart.jsx
@@ -1,7 +1,7 @@
 const DashboardCart = () => {
     return (
         <div>
-            hello
+            
         </div>
     );
 };
